1493f34c7ebd7860332d85b90eed54ea245667f6
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-03 22:47:51 +0530

    #all function added

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
+const routes = require('./routers')
 const db = require('./helpers/connection')
 
 app.use(express.json())
 
 const PORT = 8000
 
-app.get('/api',(req, res) => {
-    res.send('Api testing')
-})
+app.use('/api',routes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
